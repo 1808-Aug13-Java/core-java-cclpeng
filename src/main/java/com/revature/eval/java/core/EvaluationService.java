6d11c80a9ec3b9a89bcf0ebd3da648feeb65f2e7
cclpeng <clpeng@ucdavis.edu>
2018-08-20 16:28:26 -0400

    most of the problems, but the prime one doenst pass

--- src/main/java/com/revature/eval/java/core/EvaluationService.java
+++ src/main/java/com/revature/eval/java/core/EvaluationService.java
@@ -1,9 +1,17 @@
 package com.revature.eval.java.core;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
 import java.time.temporal.Temporal;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Vector;
+import java.util.concurrent.TimeUnit;
 
 // Functions to just memorize for future
 /*
@@ -23,7 +31,20 @@ import java.util.Map;
  * 				// "jason".substring(0, 3) ===> "jas" ===> 0th, 1st, 2nd (But NOT 3rd)
  * 
  * String already has a toLowerCase() method ===> s.toLower/UpperCase();
+ * 
+ * Change character to its ascii value? Just type cast it into an int ==> (int) 'a'
+ * Regex what does split("[_]+") do?   <====the _ means capital letter
+ * 
+ * How to get integer version of '1', '3', '5,', etc.?    ===> their unicode is also just 1, 3, 5
+ * 																just use character or (int) it
+ *							Can also just do char - '0'
+ *							eg. '0' - '0' is 48 - 48 == 0 :D
+ *							    '1' - '0'    49 - 48 == 1
+ *How to convert String "-456" into integer?
+ *							int saved = Integer.valueOf("-456");
+ *
  */
+
 
 
 public class EvaluationService {
@@ -250,25 +271,13 @@ public class EvaluationService {
 		for(int i = 0; i < string.length(); i++)
 			if(Character.isDigit(string.charAt(i)))
 			{
-				if(index == 0)
+				if(index == 0 && string.charAt(i) == '1')
 					flagLeading1 = true;
 				index++;
 			}
 		
 		if(index > 11)	//more than 11 numbers, gen exception
-		{
 			throw new IllegalArgumentException("Too many numbers");
-			//FIXME ASK CAROLYN I dont know why this passes the test, or what it does.
-			//How come what I tried before below doesn't work?
-//			try {
-//				throw new IllegalArgumentException("Too many numbers");
-//			}	catch(IllegalArgumentException e) {
-//				System.out.println("yo");
-//				e.getStackTrace();
-//				return null;
-//			}
-//			System.out.println(index);			//this is apparently unreachable code bcuz of throw
-		}
 		
 		if(index == 11 && !flagLeading1)
 			throw new IllegalArgumentException("11 numbers is too many if the first is not 1");
@@ -280,7 +289,8 @@ public class EvaluationService {
 		for(int i = 0; i < string.length(); i++)
 			if(Character.isDigit(string.charAt(i)))
 				charCleaned[pos++] = string.charAt(i);
-		charCleaned[pos] = '\0';			//getting spaces afterwards?? :( cuz same length as string FIXME question
+		//FIXMME.....THIS WAS GIVING ME SO MANY ERRORS >:OOO SOO DUMB
+//		charCleaned[pos] = '\0';			//getting spaces afterwards?? :( cuz same length as string FIXME question
 		
 		//remove any '1' if index == 11 
 		if(charCleaned[0] == '1' && index == 11)
@@ -360,24 +370,92 @@ public class EvaluationService {
 	 * binary search is a dichotomic divide and conquer search algorithm.
 	 * 
 	 */
-	static class BinarySearch<T> {
+	static class BinarySearch<T extends Comparable> {
 		private List<T> sortedList;
 
-		public int indexOf(T t) {
-			// TODO Write an implementation for this method declaration
-			return 0;
+		public int helper(T t, int first, int last)
+		{
+			if(last - first == 0)
+				if(sortedList.get(first).toString().equals(t.toString()))
+					return first;
+				else
+					return -1;
+			
+			int halfway = (first + last) / 2;
+			String tString = t.toString();
+			String elemStr = sortedList.get(halfway).toString();
+			if( elemStr.equals(tString) )
+				return halfway;
+			else
+			{ //make the sorted list in half
+				if(halfway == 0)
+					return -1;
+				if( t.compareTo(sortedList.get(halfway)) < 0 ) //search key less than middle elem
+					return helper(t, first, halfway - 1);
+				
+				else //return other half
+					return helper(t, halfway + 1, last);
+			}
 		}
+		public int indexOf(T t) {
+			if(sortedList.isEmpty())
+				return -1;					//not found
+			
+			return helper(t, 0, sortedList.size() - 1);
+			
+			/*
+			int halfway = sortedList.size() / 2;
+			String tString = t.toString();
+			System.out.println(tString);
+			String elemStr = sortedList.get(halfway).toString();
+			System.out.println(elemStr);
+			if( elemStr.equals(tString) )
+				return halfway;
+			else
+			{ //make the sorted list in half
+				if( t.compareTo(sortedList.get(halfway)) < 0 ) //search key less than middle elem
+				{
+					System.out.println("goes here?");
+					List<T> half1 = new LinkedList<T> ();
+					for(int i = 0; i <= halfway; i++)
+						half1.add(sortedList.get(i));
+					System.out.println(half1);
+					setSortedList(half1);
+					return indexOf(t);
+				}
+				
+				else //return other half
+				{
+					System.out.println("goes here???");
+					List<T> half2 = new LinkedList<T> ();
+					for(int i = halfway; i < getSortedList().size(); i++)
+						half2.add(sortedList.get(i));
+					System.out.println(half2);
+					setSortedList(half2);
+					return indexOf(t);
+				}
+			}
+			
+//			return 0;   //i will be the position...
+ 
+ 		*/
+		}
+		
+		/* have a priivate helper method binSearch(int first, int last, int[] arr)
+		 * calculate midpoint
+		 * if 
+		 */
 
-		public BinarySearch(List<T> sortedList) {
+		public BinarySearch(List<T> sortedList) { //constructor
 			super();
 			this.sortedList = sortedList;
 		}
 
-		public List<T> getSortedList() {
+		public List<T> getSortedList() {  //access the list
 			return sortedList;
 		}
 
-		public void setSortedList(List<T> sortedList) {
+		public void setSortedList(List<T> sortedList) { //reassign the list
 			this.sortedList = sortedList;
 		}
 
@@ -473,10 +551,35 @@ public class EvaluationService {
 	 * 
 	 * @param l
 	 * @return
-	 */
+	 */								//FIXME idk wats wrong...skipping
 	public List<Long> calculatePrimeFactorsOf(long l) {
-		// TODO Write an implementation for this method declaration
-		return null;
+		List<Long> primes = new LinkedList<Long> ();
+		primes.add((long) 2);
+		
+		List<Long> p = new LinkedList<Long> ();
+		p.add(l);
+		System.out.println("This is long: "+ p.toString());
+		
+		//generate primes from 2 to l
+		for(long possiblePrime = 3; possiblePrime < l; possiblePrime++)
+		{
+			for(Long prime : primes)
+			{
+				if(possiblePrime % prime != 0) 	//uneven division
+				{
+					primes.add((long)possiblePrime);	//add to the list
+					System.out.println("This is long: "+ primes.toString());
+				}
+			}
+		}
+		String a = primes.toString();
+		System.out.println(a);
+		
+//		for(Long prime : primes)
+//			if(l % prime != 0)	// if prime is not a factor of l
+//				primes.remove(prime);
+
+		return primes;
 	}
 
 	/**
@@ -514,8 +617,32 @@ public class EvaluationService {
 		}
 
 		public String rotate(String string) {
-			// TODO Write an implementation for this method declaration
-			return null;
+			char[] copy = new char[string.length()];
+		
+			for(int i = 0; i < string.length(); i++)
+			{
+				char curr = string.charAt(i);	//save the character value
+				int currInt = (int) curr;	//save the ascii code value of character
+				
+				if( !Character.isAlphabetic(curr) ) //numbers, space, punctuation 
+					copy[i] = curr;					//no change
+				
+				//deal with uppercase letters
+				else if (currInt < (int)'Z')	
+					//would adding the key stay in bounds?
+					if(currInt + key > 'Z')
+						copy[i] = (char) (currInt + key - 26 );
+					else
+						copy[i] = (char) (currInt + key);	
+				
+				else //currInt is lowercase
+					if(currInt + key > 'z')
+						copy[i] = (char) (currInt + key - 26 );
+					else
+						copy[i] = (char) (currInt + key);
+			}
+			
+			return String.valueOf(copy);	
 		}
 
 	}
@@ -533,8 +660,34 @@ public class EvaluationService {
 	 * @return
 	 */
 	public int calculateNthPrime(int i) {
-		// TODO Write an implementation for this method declaration
-		return 0;
+		int index = 1;
+		int[] primes = new int[i];	//array will be of i length. each index holds the next prime
+		boolean soFarStillPrime = true;
+		if(i == 0)
+			throw new IllegalArgumentException("No 0th prime. First prime starts at 2.");
+		
+		if(i == 1)
+			return 2;
+		
+		//otherwise do regular
+		primes[0] = 2;
+		for(int num = 3; index < i ; num++)//while array not full with primes yet
+		{
+			for(int p = 0; p < index; p++)	//no for each, if not filled yet completely
+				if(num % primes[p] == 0) // current num isn't prime, since divisible by other prime
+				{
+					soFarStillPrime = false;
+					break;
+				}
+			
+			if(soFarStillPrime == true)	//never got marked as false in for
+				primes[index++] = num;
+			else		//marked as false, so reset
+				soFarStillPrime = true;
+		}
+		
+		
+		return primes[i - 1];		//rtn primes[i - 1];
 	}
 
 	/**
@@ -547,8 +700,9 @@ public class EvaluationService {
 	 * the second-last, and so on.
 	 * 
 	 * An Atbash cipher for the Latin alphabet would be as follows:
-	 * 
-	 * Plain: abcdefghijklmnopqrstuvwxyz Cipher: zyxwvutsrqponmlkjihgfedcba It is a
+	 *         0123456789012 3456789012345   0->25 for index, 26 numbers/length
+	 * Plain:  abcdefghijklm nopqrstuvwxyz 
+	 * Cipher: zyxwvutsrqpon mlkjihgfedcba It is a
 	 * very weak cipher because it only has one possible key, and it is a simple
 	 * monoalphabetic substitution cipher. However, this may not have been an issue
 	 * in the cipher's time.
@@ -570,8 +724,41 @@ public class EvaluationService {
 		 * @return
 		 */
 		public static String encode(String string) {
-			// TODO Write an implementation for this method declaration
-			return null;
+			int length;
+//			string = string.toLowerCase();
+			String cleanedString = "";
+			//remove original spaces and punctuation from string with split, Array
+			String[] cleanedChunks = string.split("[ ,!.?()*%#-]+");
+			//DONT PUT AN UNDERSCORE unless u wanna parse uppercase ltters lol
+			
+			for(String chunk : cleanedChunks) //loop through array to add up into 1 string again
+				cleanedString += chunk;
+				
+			//calculate length of copy array. (string's length + length/5)
+			length = cleanedString.length();
+			char[] charCopy = new char[length + length / 5];
+			
+			int i, ind = 0;	//ind will track charCopy indices, which go up differently
+			for(i = 0; i < length; i++)	//go through character by character and copy/edit
+			{		
+				//every 5 characters add a space
+				if(i > 0 && i % 5 == 0)
+					charCopy[ ind++ ] = ' ';	//process this space index, then move on
+				
+				if( Character.isDigit(cleanedString.charAt(i)) ) //if isDigit, just copy 
+					charCopy[ ind++ ] = cleanedString.charAt(i);
+				//else is character, copy a toLowerCase(), ciphered version
+				else
+				{	//make things simpler, work with just lower cased version
+					char saved = Character.toLowerCase( cleanedString.charAt(i) );
+					//(25 - (letter - 'a')) + 'a'		
+					int aAsciiStartIndex = 'a';			//saving the ascii index of 'a' for readability
+					int index = saved - aAsciiStartIndex; //get number 0 -> 25 which is letter of saved
+					saved = (char) (25 - index + aAsciiStartIndex);
+					charCopy[ ind++ ] = saved;
+				}
+			}
+			return String.valueOf(charCopy).trim();
 		}
 
 		/**
@@ -581,8 +768,25 @@ public class EvaluationService {
 		 * @return
 		 */
 		public static String decode(String string) {
-			// TODO Write an implementation for this method declaration
-			return null;
+			//remove spaces
+			String cleaned = "";
+			String[] tokens = string.split("[ ]+");	//
+			for (String t: tokens)
+				cleaned += t;
+			
+			char[] deciphered = new char[cleaned.length()];
+			for(int i = 0; i < cleaned.length(); i++)
+				if(Character.isDigit(cleaned.charAt(i)))
+					deciphered[i] = cleaned.charAt(i);	//copy opver numbers
+				else //alphabet
+				{
+					int aAsciiStartIndex = 'a';			//saving the ascii index of 'a' for readability
+					int index = cleaned.charAt(i) - aAsciiStartIndex; //get number 0 -> 25 which is letter of saved
+					char saved = (char) (25 - index + aAsciiStartIndex);
+					deciphered[i] = saved;
+				}
+			
+			return String.valueOf(deciphered);
 		}
 	}
 
@@ -609,8 +813,44 @@ public class EvaluationService {
 	 * @return
 	 */
 	public boolean isValidIsbn(String string) {
-		// TODO Write an implementation for this method declaration
-		return false;
+	//clean the string: 
+		String nodash = "";
+		String[] chunks = string.split("[-]+"); //remove -'s
+		for(String chunk : chunks)
+			nodash += chunk;
+		
+		if(nodash.length() != 10)	//must have 9 digits and (1 more digit or 'X')
+			return false;
+		
+		//only numbers until last char check
+		int i, length = nodash.length();
+		for(i = 0; i < length - 1; i++)
+			if( !Character.isDigit( nodash.charAt(i) ) ) 
+				return false; 			
+		if(!Character.isDigit( nodash.charAt(i) ) )	//last digit not digit AND not 'X' check
+		   if(nodash.charAt(i) != 'X')				
+			   return false;       
+		
+		
+		//plug into formula
+		int sum = 0;
+		for(int formula = 10; formula > 1; formula--)	//formula goes 10 -> 2
+		{
+			sum += formula * Character.getNumericValue( nodash.charAt(10 - formula) );
+		}
+		
+		//deal with last character and last string item	
+		char lastChar = nodash.charAt(length - 1);
+		if(lastChar != 'X')
+			sum += Character.getNumericValue(lastChar); 		//last * 1
+		else		//treat 'X' like 10
+			sum += 10;
+				
+		if(sum % 11 == 0)
+			return true;
+		else
+			return false;
+		
 	}
 
 	/**
@@ -627,8 +867,43 @@ public class EvaluationService {
 	 * @return
 	 */
 	public boolean isPangram(String string) {
-		// TODO Write an implementation for this method declaration
-		return false;
+		Vector v = new Vector();
+		String formatted, nospace = "";
+		
+		if(string == null)
+			return false;
+		
+		String[] tokens = string.split("[ ]+");
+		for(String t : tokens)
+			nospace += t;
+		
+		if(nospace.length() < 26) //need at least all the characters	
+			return false;
+		
+		//lowercase everything
+		formatted = nospace.toLowerCase();
+		v.add(formatted.charAt(0));		//add the first char to vector to have something to compare
+		
+		boolean alreadyCounted = false;
+		for(int i = 1; i < formatted.length(); i++)
+		{
+			for(int vIndex = 0; vIndex < v.size(); vIndex++)
+				if( formatted.charAt(i) == (char) v.get(vIndex) )	//returns Object, just typecast
+				{
+					alreadyCounted = true;
+					break;
+				}
+			
+			if(alreadyCounted == true)		//don't change the vector
+				alreadyCounted = false;		//resetting flag
+			else						//unique letter so add to vector
+				v.add( formatted.charAt(i) );
+		}
+		
+		if(v.size() < 26)
+			return false;
+		else
+			return true;
 	}
 
 	/**
@@ -641,7 +916,31 @@ public class EvaluationService {
 	 */
 	public Temporal getGigasecondDate(Temporal given) {
 		// TODO Write an implementation for this method declaration
-		return null;
+		System.out.println(LocalDateTime.now());
+		System.out.println(LocalDateTime.now().getMonth());
+		
+//	given.plus
+		if(given instanceof LocalDateTime)
+		{
+			LocalDateTime n = (LocalDateTime) given;
+			return n.plus(1000000000, ChronoUnit.SECONDS);
+			
+		}
+		if(given instanceof LocalDate)
+		{
+			System.out.println("here");
+			LocalDate n = (LocalDate) given;
+			LocalDateTime b = n.atStartOfDay();
+			return (b.plus(1000000000, ChronoUnit.SECONDS));
+			
+//			return n.plus(1000000000, ChronoUnit.SECONDS);
+		}
+//		rn n.plus(1000000000, ChronoUnit.SECONDS);
+		
+		System.out.println("Wasnt caught");
+		return given.plus((long) 1000000000, ChronoUnit.DAYS);
+//		System.out.println(LocalDateTime.of(2043, Month.JANUARY, 1, 1, 46, 40));
+//		return null;
 	}
 
 	/**
@@ -658,8 +957,21 @@ public class EvaluationService {
 	 * @return
 	 */
 	public int getSumOfMultiples(int i, int[] set) {
-		// TODO Write an implementation for this method declaration
-		return 0;
+		ArrayList<Integer> toBeSummed = new ArrayList<Integer> ();
+		//go up to i, but not including i. so 1 --> i-1;
+		for(int number = 1; number < i; number++)
+			for(int s = 0; s < set.length; s++)		//nested forloop through set array
+			//if is multiple, add to sum array and break;
+				if(number % set[s] == 0)
+				{
+					toBeSummed.add(number);
+					break;	//dont want to add multiple times
+				}
+		
+		int sum = 0;
+		for(Object integer : toBeSummed)
+			sum += (Integer) integer;
+		return sum;
 	}
 
 	/**
@@ -699,10 +1011,58 @@ public class EvaluationService {
 	 * @return
 	 */
 	public boolean isLuhnValid(String string) {
-		// TODO Write an implementation for this method declaration
-		return false;
+		///rtn false for !isDigit, !isWhiteSpace
+		for(int i = 0; i < string.length(); i++)
+			if( !Character.isDigit( string.charAt(i) ) )
+				if( !Character.isWhitespace( string.charAt(i) ) )
+					return false;
+		
+		String clean = parse(string, "[ ]+");
+		
+		//array with changes
+		int[] doubled = new int [clean.length()];
+		int count = 1;		
+		
+		//read characters from (length-2) of string to 0th. Decrement by 2
+		for(int i = (clean.length() - 1); i >= 0; i --)
+		{
+			int toInt = clean.charAt(i) - '0'; //for each character, get int version
+			if(count % 2 == 0) //every other
+			{
+				//double it, and check if in bounds
+				toInt *= 2;
+				if(toInt > 9)
+					toInt = toInt - 9;
+				//resave into other array
+				doubled[i] = toInt;
+			}
+			
+			else //just copy
+				doubled[i] = toInt;
+			
+			count++;
+		}	//for()
+		
+		//sum up doubled and see if it's evenly divisible by 10
+		int sum = 0;
+		for(int i : doubled)
+			sum += i;
+		
+		if(sum % 10 == 0)
+			return true;
+		return false;		
 	}
 
+	public String parse(String string, String regexpression)
+	{
+		//clean up string
+		String clean = "";
+		String[] despacedChunks = string.split(regexpression);
+		for(String chunk : despacedChunks)
+			clean += chunk;
+		return clean;
+	}
+	
 	/**
 	 * 20. Parse and evaluate simple math word problems returning the answer as an
 	 * integer.
@@ -731,8 +1091,62 @@ public class EvaluationService {
 	 * @return
 	 */
 	public int solveWordProblem(String string) {
-		// TODO Write an implementation for this method declaration
+		int[] twoNumbers = new int[2];
+		int index = 0;
+		String[] operations = new String[5]; //up to 5, but we're only doing 1
+		int indexOp = 0;
+		
+		//parse the string for spaces
+		String[] words = string.split("[ ?]+");
+		for(String word : words)
+			if( checkIsNumber(word) )	//process a number
+				twoNumbers[index++] = Integer.valueOf(word);
+			else if(returnKeyword(word) != null) //process a keyword
+				operations[indexOp++] = returnKeyword(word);
+			else			//anything else
+				continue;
+		
+		//now do operation
+		int count = 0, countOp = 0;
+		int rounds = index / 2;
+		
+		for(int i = 0; i < rounds; i++)
+		{
+			switch(operations[countOp])
+			{
+			case "plus":
+				return twoNumbers[count++] + twoNumbers[count++];
+			case "minus":
+				return twoNumbers[count++] - twoNumbers[count++];
+			case "multiplied":
+				return twoNumbers[count++] * twoNumbers[count++];
+			case "divided":
+				return twoNumbers[count++] / twoNumbers[count++];
+			}
+			
+			countOp++;
+		}
+		
 		return 0;
+	}
+	
+	public String returnKeyword(String s)
+	{
+		String[] keywords = {"plus", "minus", "multiplied", "divided"};
+		for(String key : keywords)
+			if(s.equals(key))
+				return key;
+		return null;
+	}
+	public boolean checkIsNumber(String string)
+	{
+		for(int i = 0; i < string.length(); i++)
+			if( !Character.isDigit( string.charAt(i) ) )
+				if(string.charAt(i) == '-')
+					return true;
+				else
+					return false;
+		return true;
 	}
 
 }
